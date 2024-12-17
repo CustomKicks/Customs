fd6906523cec8a58974ac9ae67bcf43486ea8379
CustomKicks <Customkicks.django@gmail.com>
2024-12-18 01:57:10 +0200

    Update settings.py

--- Customs/settings.py
+++ Customs/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '# Secret Key'
+SECRET_KEY = 'django-insecure-8%g&cru09l5k89!v&+%s99o9o_3_m=dl^9enu@rmztn2'
 #os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
